7b447b8ace2af17c76594aae6759d3dd3b395b3a
nogaye <nogayediop973@gmail.com>
2022-06-23 12:21:31 +0000

    création  formulaire pour ajout nouveau servers et conception du filtrage des servers et enfin la possibilité de télécharger la version excel du projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject, catchError, map, Observable, of, startWith } from 'rxjs';
+import { NgForm } from '@angular/forms';
+import { BehaviorSubject, catchError, map, Observable, of, pipe, startWith } from 'rxjs';
 import { DataState } from './enum/data.state.enum';
 import { Status } from './enum/status.enum';
 import { AppState } from './interface/app-state';
 import { CustomResponse } from './interface/custom-response';
+import { Server } from './interface/server';
+
 import { ServerService } from './service/server.service';
 
 @Component({
@@ -20,8 +23,10 @@ export class AppComponent implements OnInit {
 
 private filterSubject = new BehaviorSubject<string>('');
 private dataSubject = new BehaviorSubject<CustomResponse>(null);
-
 filterStatus$ = this.filterSubject.asObservable();
+
+private isLoading = new BehaviorSubject<boolean>(false);
+isLoading$= this.isLoading.asObservable();
 
 constructor(private serverService : ServerService) {
 console.log(Status);
@@ -33,7 +38,7 @@ this.appState$ = this.serverService.servers$
 .pipe(
   map(response => {
     this.dataSubject.next(response);
-return { dataState: DataState.LOADED_STATE, appData:response}
+return { dataState: DataState.LOADED_STATE, appData:{...response,data: { servers:response.data.servers.reverse()}}}
   }
 ) ,
 startWith({dataState: DataState.LOADING_STATE}),
@@ -62,7 +67,7 @@ this.appState$ = this.serverService.ping$(ipAddress)
 );
 }
 
-filterServers(status: Status): void
+filterServers(status: any): void
 {
   this.appState$ = this.serverService.filter$(status, this.dataSubject.value)
 .pipe(
@@ -75,5 +80,61 @@ filterServers(status: Status): void
   })
 );
 }
+saveServer(serverForm:NgForm):void
+{
+  this.isLoading.next(true);
+  this.appState$ = this.serverService.save$(serverForm.value as Server)
+.pipe(
+  map(response => {
+   this.dataSubject.next(
+    {...response, data:{ servers: [response.data.server,...this.dataSubject.value.data.servers]}}
+   );
+document.getElementById('closeModal').click();
+this.isLoading.next(false);
+serverForm.resetForm({status:this.Status.SERVER_DOWN});
+    return { dataState: DataState.LOADED_STATE, appData:this.dataSubject.value}
+  }),
+  startWith({dataState: DataState.LOADED_STATE, appData:this.dataSubject.value}),
+  catchError((error:string ) => {
+    this.isLoading.next(false);
+    return of({ dataState: DataState.ERROR_STATE, error})
+  })
+);
+}
+deleteServer(server:Server):void
+{
 
+this.appState$ = this.serverService.delete$(server.id)
+.pipe(
+  map(response => {
+
+   this.dataSubject.next(
+    {...response,data:{
+      servers:this.dataSubject.value.data.servers.filter(s =>s.id !== server.id)
+    }}
+   )
+    return { dataState: DataState.LOADED_STATE, appData:this.dataSubject.value}
+  }),
+  startWith({dataState: DataState.LOADED_STATE, appData:this.dataSubject.value}),
+  catchError((error:string ) => {
+    this.filterSubject.next('');
+    return of({ dataState: DataState.ERROR_STATE, error})
+  })
+);
+}
+printReport():void{
+
+  //POUR IMPRIMER LA PAGE
+ // window.print();
+
+let dataType = 'application/vnd.ms.excel.sheet.macroEnabled.12';
+let tableSelect = document.getElementById('servers');
+let tableHtml = tableSelect.outerHTML.replace(/ /g, '%20');
+let downloadLink = document.createElement('a');
+document.body.appendChild(downloadLink);
+downloadLink.href = 'data:' + dataType + ',' +tableHtml;
+downloadLink.download = 'server-report.xls';
+downloadLink.click();
+document.body.removeChild(downloadLink);
+}
 }
